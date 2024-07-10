d25e2ceefafaf67f0c92e82281d0f7898ddc9602
Mostafa Farrag <moah.farag@gmail.com>
2024-07-10 16:11:42 +0200

    move files from tests to examples

--- examples/run-model.py
+++ examples/run-model.py
@@ -1,8 +1,5 @@
 import os
-import torch
-from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 from dotenv import load_dotenv
-
 load_dotenv(f".env")
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 from llm.model import ChatModel, list_models
@@ -10,17 +7,10 @@ from llm.model import ChatModel, list_models
 models = list_models(limit=10, author="openai")
 # %%
 model_dir = r"\\MYCLOUDEX2ULTRA\research\llm"
-model_id = "google/gemma-2b-it"
+model_id = "meta-llama/Meta-Llama-3-8B"
 model = ChatModel(
     model_id=model_id, device="cuda", access_token=ACCESS_TOKEN, model_dir=model_dir
 )
-# sentence_model = ChatModel(model_id="sentence-transformers/all-mpnet-base-v2", device="cuda", model_dir=model_dir, access_token=ACCESS_TOKEN)
-# %% check if cuda available
-print(torch.cuda.is_available())
-print("CUDA available: ", torch.cuda.is_available())
-print("CUDA device count: ", torch.cuda.device_count())
-print("Current CUDA device: ", torch.cuda.current_device())
-print("CUDA device name: ", torch.cuda.get_device_name(torch.cuda.current_device()))
 # %%
 print(model.generate(question="How are you?", context=""))
 """
@@ -41,28 +31,28 @@ print(response)
 # %% Document Loader and Text Splitter
 from llm.datasource import DataSource
 from llm.encoder import Encoder
+encoder = Encoder(model_id="sentence-transformers/all-MiniLM-L12-v2", device="cuda", model_dir=model_dir)
 
 datasource = DataSource(dtype="pdf", chunk_size=256, overlap=25, model="sentence-transformers/all-MiniLM-L12-v2")
-paper_name = ["paper.pdf"]
-# paper_name = [rf"wf45t6000a_series.pdf"]
+# paper_name = ["paper.pdf"]
+paper_name = [rf"wf45t6000a_series.pdf"]
 docs = datasource.load_data(paper_name)
-
-encoder = Encoder(model_id="sentence-transformers/all-MiniLM-L12-v2", device="cuda", model_dir=model_dir)
 # %% Vector Database
 """
 Faiss is a vector database library from Meta’s fundamental AI research team for efficient similarity search and
 clustering of dense vectors. Using LangChain’s community integration, we can use our docs variable from the text
 splitter to create a Faiss database in RAM.
 """
-
-from langchain_community.vectorstores import FAISS
-from langchain_community.vectorstores.utils import DistanceStrategy
-
-faiss_db = FAISS.from_documents(
-    docs, encoder.embedding_function, distance_strategy=DistanceStrategy.COSINE
-)
-
-retrieved_docs = faiss_db.similarity_search("what does return period means", k=5)
-
-retrieved_docs[0].page_content
-
+from llm.database import Faiss
+DB = Faiss(docs, encoder.embedding_function)
+user_prompt = "how to select a location"
+retrieved_docs = DB.similarity_search(user_prompt, k=5)
+# retrieved_docs = DB.similarity_search("how do i drain the filter", k=10)
+len(retrieved_docs)
+#%%
+user_prompt = "what is a return period"
+retrieved_docs = """A return period, also known as a recurrence interval or repeat interval, is an average time or an 
+estimated average time between events such as earthquakes, floods,[1] landslides,[2] or river discharge flows to occur.
+It is a statistical measurement typically based on historic data over an extended period, and is used usually for risk analysis. Examples include deciding whether a project should be allowed to go forward in a zone of a certain risk or designing structures to withstand events with a certain return period. The following analysis assumes that the probability of the event occurring does not vary over time and is independent of past events."""
+#%%
+answer = model.generate(user_prompt, context=retrieved_docs, max_new_tokens=512)

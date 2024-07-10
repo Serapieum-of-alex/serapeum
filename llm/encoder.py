5cf10afec29e9ec9d4802ad37cf5e6fae492959e
Mostafa Farrag <moah.farag@gmail.com>
2024-07-10 16:11:03 +0200

    check if model_dir exists in the `encoder` class

--- llm/encoder.py
+++ llm/encoder.py
@@ -1,15 +1,19 @@
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
+from pathlib import Path
+
 
 class Encoder:
-
+    """Encoder"""
     def __init__(
             self, model_id: str = "sentence-transformers/all-MiniLM-L12-v2", device: str = "cpu",
             model_dir: str = None,
     ):
+        if not Path(model_dir).exists():
+            raise FileNotFoundError(f"Model directory '{model_dir}' does not exist.")
+
         self.embedding_function = HuggingFaceEmbeddings(
             model_name=model_id,
             cache_folder=model_dir,
             model_kwargs={"device": device},
         )
-
 

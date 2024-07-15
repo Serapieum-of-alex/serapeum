bb95788e088eb5df9da2b1720feb6535e25f8951
Mostafa Farrag <moah.farag@gmail.com>
2024-07-16 00:53:33 +0200

    update encoder script

--- examples/encoder.py
+++ examples/encoder.py
@@ -15,3 +15,18 @@ encoder = HuggingFaceEmbeddings(
 embeddings = encoder.embed_query("How are you?")
 print(embeddings)
 len(embeddings)
+# %%
+import numpy as np
+
+
+q = encoder.embed_query("What is an apple?")
+z1 = encoder.embed_query(
+    "An apple is a round, edible fruit produced by an apple tree (Malus spp., among them the domestic or orchard apple; Malus domestica)."
+)  # from wikipedia
+z2 = encoder.embed_query(
+    "The cat (Felis catus), commonly referred to as the domestic cat or house cat, is the only domesticated species in the family Felidae."
+)  # from wikipedia
+
+print(np.dot(q, z1) / (np.linalg.norm(q) * np.linalg.norm(z1)))
+
+print(np.dot(q, z2) / (np.linalg.norm(q) * np.linalg.norm(z2)))

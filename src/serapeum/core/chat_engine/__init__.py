f113cf2f4065efce4be86394177908a5bc8b58f9
Mostafa Farrag <moah.farag@gmail.com>
2025-09-23 00:13:27 +0200

    add initial implementation for chat engine types and utility methods

---
+++ src/serapeum/core/chat_engine/__init__.py
@@ -0,0 +1,15 @@
+# from serapeum.core.chat_engine.condense_plus_context import (
+#     CondensePlusContextChatEngine,
+# )
+# from serapeum.core.chat_engine.condense_question import (
+#     CondenseQuestionChatEngine,
+# )
+# from serapeum.core.chat_engine.context import ContextChatEngine
+# from serapeum.core.chat_engine.simple import SimpleChatEngine
+
+__all__ = [
+    # "SimpleChatEngine",
+    # "CondenseQuestionChatEngine",
+    # "ContextChatEngine",
+    # "CondensePlusContextChatEngine",
+]

67602e2f1bfc66586e621ba887a3b23dc5697a15
Mostafa Farrag <moah.farag@gmail.com>
2025-09-27 22:23:07 +0200

    add truncate text function

--- src/serapeum/core/utils.py
+++ src/serapeum/core/utils.py
@@ -52,3 +52,10 @@ def resolve_binary(
         return BytesIO(response.content)
 
     raise ValueError("No valid source provided to resolve binary data!")
+
+
+def truncate_text(text: str, max_length: int) -> str:
+    """Truncate text to a maximum length."""
+    if len(text) <= max_length:
+        return text
+    return text[: max_length - 3] + "..."

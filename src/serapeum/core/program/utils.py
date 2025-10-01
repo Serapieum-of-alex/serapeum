166cc4d5fd655035349e4791397fd6510d203fcf
Mostafa Farrag <moah.farag@gmail.com>
2025-10-01 22:05:58 +0200

    remove un-used function

--- src/serapeum/core/program/utils.py
+++ src/serapeum/core/program/utils.py
@@ -32,28 +32,6 @@ def create_flexible_model(model: Type[BaseModel]) -> Type[FlexibleModel]:
     )  # type: ignore
 
 
-def create_list_model(base_cls: Type[BaseModel]) -> Type[BaseModel]:
-    """Create a list version of an existing Pydantic object."""
-    # NOTE: this is directly taken from
-    # https://github.com/jxnl/openai_function_call/blob/main/examples/streaming_multitask/streaming_multitask.py
-    # all credits go to the openai_function_call repo
-
-    name = f"{base_cls.__name__}List"
-    list_items = (
-        List[base_cls],  # type: ignore
-        Field(
-            default_factory=list,  # type: ignore
-            repr=False,
-            description=f"List of {base_cls.__name__} items",
-        ),
-    )
-
-    new_cls = create_model(name, items=list_items)
-    new_cls.__doc__ = f"A list of {base_cls.__name__} objects. "
-
-    return new_cls
-
-
 def get_program_for_llm(
     output_cls: Type[BaseModel],
     prompt: BasePromptTemplate,

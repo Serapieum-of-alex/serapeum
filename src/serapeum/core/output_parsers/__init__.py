c2a51bc01f0d89364450665dc7caac1d27dac9d9
Mostafa Farrag <moah.farag@gmail.com>
2025-10-04 20:50:21 +0200

    clean

--- src/serapeum/core/output_parsers/__init__.py
+++ src/serapeum/core/output_parsers/__init__.py
@@ -2,10 +2,8 @@
 
 from serapeum.core.output_parsers.models import BaseOutputParser
 from serapeum.core.output_parsers.pydantic import PydanticOutputParser
-from serapeum.core.output_parsers.selection import SelectionOutputParser
 
 __all__ = [
     "BaseOutputParser",
     "PydanticOutputParser",
-    "SelectionOutputParser",
 ]

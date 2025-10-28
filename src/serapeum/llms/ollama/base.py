a6a73db0fbd7de71d98d1c4fe8fbc1bea487f423
Mostafa Farrag <moah.farag@gmail.com>
2025-10-28 20:13:48 +0100

    fix error in the missed process_streaming_objects replacement

--- src/serapeum/llms/ollama/base.py
+++ src/serapeum/llms/ollama/base.py
@@ -607,13 +607,13 @@ class Ollama(FunctionCallingLLM):
                 cur_objects = None
                 async for response in response_gen:
                     try:
-                        objects = process_streaming_objects(
-                            response,
-                            output_cls,
-                            cur_objects=cur_objects,
-                            allow_parallel_tool_calls=False,
+                        processor = StreamingObjectProcessor(
+                            output_cls=output_cls,
                             flexible_mode=True,
+                            allow_parallel_tool_calls=False,
                         )
+                        objects = processor.process(response, cur_objects)
+
                         cur_objects = (
                             objects if isinstance(objects, list) else [objects]
                         )

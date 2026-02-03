4cbf6c5ee1addabb9bf5b4948d16e56c7d764b07
Mostafa Farrag <moah.farag@gmail.com>
2026-02-03 21:10:28 +0100

    add `stream_completion_response_to_chat_response` function to be refactored later

--- serapeum-core/src/serapeum/core/base/llms/models.py
+++ serapeum-core/src/serapeum/core/base/llms/models.py
@@ -453,6 +453,25 @@ CompletionResponseGen = Generator[CompletionResponse, None, None]
 CompletionResponseAsyncGen = AsyncGenerator[CompletionResponse, None]
 
 
+def stream_completion_response_to_chat_response(
+    completion_response_gen: CompletionResponseGen,
+) -> ChatResponseGen:
+    """Convert a stream completion response to a stream chat response."""
+
+    def gen() -> ChatResponseGen:
+        for response in completion_response_gen:
+            yield ChatResponse(
+                message=Message(
+                    role=MessageRole.ASSISTANT,
+                    content=response.text,
+                    additional_kwargs=response.additional_kwargs,
+                ),
+                delta=response.delta,
+                raw=response.raw,
+            )
+
+    return gen()
+
 class Metadata(BaseModel):
     """Provider and model capabilities and defaults metadata."""
 

3c14a89873df8e6d77e41660e3f48032a61b7166
Mostafa Farrag <moah.farag@gmail.com>
2026-02-04 15:24:48 +0100

    add `stream_completion_response_to_chat_response` to llms/models.py

--- serapeum-core/src/serapeum/core/base/llms/models.py
+++ serapeum-core/src/serapeum/core/base/llms/models.py
@@ -489,3 +489,23 @@ class Metadata(BaseModel):
         description="The role this specific LLM provider"
         "expects for system prompt. E.g. 'SYSTEM' for OpenAI, 'CHATBOT' for Cohere",
     )
+
+
+def stream_completion_response_to_chat_response(
+    completion_response_gen: CompletionResponseGen,
+) -> ChatResponseGen:
+    """Convert a stream completion response to a stream chat response."""
+
+    def gen() -> ChatResponseGen:
+        for response in completion_response_gen:
+            yield ChatResponse(
+                message=Message(
+                    role=MessageRole.ASSISTANT,
+                    content=response.text,
+                    additional_kwargs=response.additional_kwargs,
+                ),
+                delta=response.delta,
+                raw=response.raw,
+            )
+
+    return gen()

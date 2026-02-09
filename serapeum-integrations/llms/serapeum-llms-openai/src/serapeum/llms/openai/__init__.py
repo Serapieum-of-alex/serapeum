96f8ad3a521d67627b185e2387aba9fa9ffc56ab
Mostafa Farrag <moah.farag@gmail.com>
2026-02-09 22:17:31 +0100

    add `openai` llm support

---
+++ serapeum-integrations/llms/serapeum-llms-openai/src/serapeum/llms/openai/__init__.py
@@ -0,0 +1,4 @@
+from serapeum.llms.openai.base import AsyncOpenAI, OpenAI, SyncOpenAI, Tokenizer
+from serapeum.llms.openai.responses import OpenAIResponses
+
+__all__ = ["OpenAI", "OpenAIResponses", "Tokenizer", "SyncOpenAI", "AsyncOpenAI"]

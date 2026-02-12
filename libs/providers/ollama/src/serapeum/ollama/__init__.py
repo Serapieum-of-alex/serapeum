c5db31937cfbae74087588db094a1d831aac4960
Mostafa Farrag <moah.farag@gmail.com>
2026-02-12 21:51:44 +0100

    correct the import in ollama

--- libs/providers/ollama/src/serapeum/ollama/__init__.py
+++ libs/providers/ollama/src/serapeum/ollama/__init__.py
@@ -1,6 +1,6 @@
 """Ollama."""
 from serapeum.ollama.embedding import OllamaEmbedding
 
-from serapeum.ollama.base import Ollama
+from serapeum.ollama.llm import Ollama
 
 __all__ = ["Ollama", "OllamaEmbedding"]

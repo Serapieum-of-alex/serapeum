822a1b95f283008ee8fe09f09c447c1b546e13b9
Mostafa Farrag <moah.farag@gmail.com>
2026-02-14 22:29:48 +0100

    add public api

--- libs/core/src/serapeum/core/embeddings/__init__.py
+++ libs/core/src/serapeum/core/embeddings/__init__.py
@@ -1,15 +1,25 @@
+from __future__ import annotations
+from serapeum.core.base.embeddings.types import (
+    NodeType,
+    BaseNode,
+    LinkedNodes,
+    NodeInfo,
+    NodeInfoType,
+    MetadataMode,
+    CallMixin
+)
 from serapeum.core.base.embeddings.base import BaseEmbedding
 from serapeum.core.embeddings.mock_embed_model import MockEmbedding
-from serapeum.core.embeddings.mock_embed_model import MockMultiModalEmbedding
-from serapeum.core.embeddings.multi_modal_base import MultiModalEmbedding
-from serapeum.core.embeddings.pooling import Pooling
-from serapeum.core.embeddings.utils import resolve_embed_model
 
 __all__ = [
+    "NodeType",
+    "BaseNode",
+    "LinkedNodes",
+    "NodeInfo",
+    "NodeInfoType",
+    "MetadataMode",
+    "CallMixin",
     "BaseEmbedding",
     "MockEmbedding",
-    "MultiModalEmbedding",
-    "MockMultiModalEmbedding",
-    "Pooling",
-    "resolve_embed_model",
+
 ]

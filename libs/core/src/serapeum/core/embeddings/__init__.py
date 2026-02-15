d079712d4d26af3d188a7c66faae5d7fc3402049
Mostafa Farrag <moah.farag@gmail.com>
2026-02-15 18:30:51 +0100

    add `NodeContentType` to the public api

--- libs/core/src/serapeum/core/embeddings/__init__.py
+++ libs/core/src/serapeum/core/embeddings/__init__.py
@@ -1,18 +1,21 @@
+"""Embedding module."""
 from __future__ import annotations
 from serapeum.core.base.embeddings.types import (
     NodeType,
+    NodeContentType,
     BaseNode,
     LinkedNodes,
     NodeInfo,
     NodeInfoType,
     MetadataMode,
-    CallMixin
+    CallMixin,
 )
 from serapeum.core.base.embeddings.base import BaseEmbedding
 from serapeum.core.embeddings.types import MockEmbedding
 
 __all__ = [
     "NodeType",
+    "NodeContentType",
     "BaseNode",
     "LinkedNodes",
     "NodeInfo",
@@ -21,5 +24,4 @@ __all__ = [
     "CallMixin",
     "BaseEmbedding",
     "MockEmbedding",
-
 ]

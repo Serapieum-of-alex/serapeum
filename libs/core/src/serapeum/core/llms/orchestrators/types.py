a6160345809aa55d48bdd589f760fa1f6fe04833
Mostafa Farrag <moah.farag@gmail.com>
2026-02-22 14:43:03 +0100

    rename the argument `output_cls` to `output_tool` in the  `ToolOrchestratingLLM` class.

--- libs/core/src/serapeum/core/llms/orchestrators/types.py
+++ libs/core/src/serapeum/core/llms/orchestrators/types.py
@@ -12,6 +12,7 @@ class BasePydanticLLM(ABC, Generic[Model]):
     """A base class for LLM-powered function that return a pydantic model."""
 
     @property
+    @abstractmethod
     def output_tool(self) -> Type[Model]:
         pass
 

eecdc5e8cf149e5469836fcbaaaec99969566cbd
Mostafa Farrag <moah.farag@gmail.com>
2026-02-28 18:26:32 +0100

    export the formatters to the llama_cpp api
    Signed-off-by: Mostafa Farrag <moah.farag@gmail.com>

--- libs/providers/llama-cpp/src/serapeum/llama_cpp/__init__.py
+++ libs/providers/llama-cpp/src/serapeum/llama_cpp/__init__.py
@@ -1,3 +1,15 @@
 from serapeum.llama_cpp.llm import LlamaCPP
+from serapeum.llama_cpp.formatters import (
+    completion_to_prompt,
+    completion_to_prompt_v3_instruct,
+    messages_to_prompt,
+    messages_to_prompt_v3_instruct,
+)
 
-__all__ = ["LlamaCPP"]
+__all__ = [
+    "LlamaCPP",
+    "messages_to_prompt",
+    "completion_to_prompt",
+    "messages_to_prompt_v3_instruct",
+    "completion_to_prompt_v3_instruct",
+]
